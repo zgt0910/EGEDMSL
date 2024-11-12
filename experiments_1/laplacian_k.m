38dd94dc7983a3c1a670a7c94e179e18f5837e75
zgt0910 <88957728+zgt0910@users.noreply.github.com>
2024-11-12 12:22:36 +0800

    Add files via upload

---
+++ experiments_1/laplacian_k.m
@@ -0,0 +1,50 @@
+function [W]=laplacian_k(samples_train,sigm)
+%%% to get the essential graph
+%%%% % Input:
+    %       - samples_train: the data matrix of size m n, where each columns is a sample
+    %               point
+    %       - sigm:Gaussian kernel
+    % Output:
+    %       - W:local similarity
+num_all=size(samples_train,2);
+distance=squareform(pdist(samples_train'));
+distance_one=zeros(num_all);
+distance_two=zeros(num_all);
+distance_sigm=zeros(num_all);
+
+for i=1:num_all
+    for j=1:num_all
+        distance_sigm(i,j)=distance(i,j)/sum(distance(i,:));
+    end
+end
+sigm_adjust=sqrt(sigm);
+
+%Find the similarity weight between any two points
+for i=1:num_all
+    for j=1:num_all
+        distance_one(i,j)=exp(-(distance(i,j)/sum(distance(i,:)))/(2*sigm_adjust^2));
+    end
+end
+%Find the similarity weight between any point and all sample points
+for i=1:num_all
+    for j=1:num_all
+        distance_two(i,j)=sum(distance_one(i,:))/num_all;
+    end
+end
+W=distance_one.*(distance_one>distance_two);
+
+
+% for i=1:num_all
+%     W(i,i)=0;
+% end
+    
+%L=diag(sum(W,2))-W;
+
+% % %求度矩阵D
+% D=diag(sum(W,2));
+% % 
+% % %求邻近图的标准的拉普拉斯矩阵
+% % L=eye(num_all)-sqrt(inv(D))*W*sqrt(inv(D));
+% W=sqrt(inv(D))*W*sqrt(inv(D));
+end
+
